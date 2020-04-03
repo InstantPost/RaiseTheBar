530c3afd9067f7c05382204dd61bf0bbf30ce0b4
Tanuj Nagpal <tanujnagpal999@gmail.com>
2020-04-03 15:48:47 +0530

    workbox test

--- src/sw.js
+++ src/sw.js
@@ -15,55 +15,55 @@ if (workbox) {
   console.log(`Boo! Workbox didn't load 😬`);
 }
 
-import { StaleWhileRevalidate, CacheFirst } from "workbox-strategies";
-import { precacheAndRoute } from "workbox-precaching";
-import { registerRoute } from "workbox-routing";
-import { skipWaiting, clientsClaim } from "workbox-core";
-const env = "dev";
-let API;
-if (env == "dev") {
-  API = "http://localhost:2000/";
-} else {
-  API = "https://instantpost.org/covid/api/";
-}
-skipWaiting();
-clientsClaim();
+// import { StaleWhileRevalidate, CacheFirst } from "workbox-strategies";
+// import { precacheAndRoute } from "workbox-precaching";
+// import { registerRoute } from "workbox-routing";
+// import { skipWaiting, clientsClaim } from "workbox-core";
+// const env = "dev";
+// let API;
+// if (env == "dev") {
+//   API = "http://localhost:2000/";
+// } else {
+//   API = "https://instantpost.org/covid/api/";
+// }
+// skipWaiting();
+// clientsClaim();
 
-precacheAndRoute(self.__WB_MANIFEST);
-registerRoute(
-  /\.(?:png|gif|jpg|svg)$/,
-  new CacheFirst({
-    cacheName: "images-cache"
-  })
-);
-registerRoute(
-  /\.(?:js|css)$/,
-  new StaleWhileRevalidate({
-    cacheName: "static-resources"
-  })
-);
-registerRoute(/.*(?:googleapis|gstatic)\.com.*$/, new StaleWhileRevalidate());
-registerRoute(
-  new RegExp(API + "volunteer/.+"),
-  new StaleWhileRevalidate({
-    cacheName: "volunteer"
-  })
-);
-registerRoute(
-  new RegExp(API + "doctor/.+"),
-  new StaleWhileRevalidate({
-    cacheName: "doctor"
-  })
-);
-registerRoute(
-  new RegExp(".+/printer/.+"),
-  new StaleWhileRevalidate({
-    cacheName: "printer"
-  })
-);
-registerRoute(
-  new RegExp(API + "commodity/.+"),
-  new StaleWhileRevalidate({
-    cacheName: "commodity"
-  })
-);
+// precacheAndRoute(self.__WB_MANIFEST);
+// registerRoute(
+//   /\.(?:png|gif|jpg|svg)$/,
+//   new CacheFirst({
+//     cacheName: "images-cache"
+//   })
+// );
+// registerRoute(
+//   /\.(?:js|css)$/,
+//   new StaleWhileRevalidate({
+//     cacheName: "static-resources"
+//   })
+// );
+// registerRoute(/.*(?:googleapis|gstatic)\.com.*$/, new StaleWhileRevalidate());
+// registerRoute(
+//   new RegExp(API + "volunteer/.+"),
+//   new StaleWhileRevalidate({
+//     cacheName: "volunteer"
+//   })
+// );
+// registerRoute(
+//   new RegExp(API + "doctor/.+"),
+//   new StaleWhileRevalidate({
+//     cacheName: "doctor"
+//   })
+// );
+// registerRoute(
+//   new RegExp(".+/printer/.+"),
+//   new StaleWhileRevalidate({
+//     cacheName: "printer"
+//   })
+// );
+// registerRoute(
+//   new RegExp(API + "commodity/.+"),
+//   new StaleWhileRevalidate({
+//     cacheName: "commodity"
+//   })
+// );
