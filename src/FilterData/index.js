c4573c31b6cc9cc49aeb455e5c71347c7efb4e1b
Tanuj Nagpal <tanujnagpal999@gmail.com>
2020-04-04 13:03:47 +0530

    Scrollbar customisations, networkFirst caching

--- src/FilterData/index.js
+++ src/FilterData/index.js
@@ -9,7 +9,7 @@ export function filter(element) {
   const FilterValue = element.innerText;
   console.log(FilterBy, FilterValue);
   fetch(`${process.env.BACKEND_URI}${endpoint}/`)
-    .then(res => {
+    .then((res) => {
       if (res.status == 200) {
         return res.json();
       } else {
@@ -17,14 +17,29 @@ export function filter(element) {
         throw new Error("..");
       }
     })
-    .then(objs => {
+    .then((objs) => {
+      objs.reverse();
       OpenModal(FilterDataTemplate);
-      objs = objs.filter(elem => {
+      objs = objs.filter((elem) => {
         return elem.data[FilterBy] == FilterValue;
       });
-      id(
-        "filter-title"
-      ).innerHTML = `Filtered results by ${FilterBy} for ${FilterValue}`;
+      let FilterTitle = "";
+      if (endpoint == "doctor") {
+        FilterTitle += "Requirements by Doctor";
+      } else if (endpoint == "printer") {
+        FilterTitle += "Manufacturer";
+      } else if (endpoint == "commodity") {
+        FilterTitle += "Commodity requirements ";
+      } else if (endpoint == "volunteer") {
+        FilterTitle += "Volunteers ";
+      }
+      if (FilterBy == "city" || FilterBy == "city_pin") {
+        FilterTitle += " in ";
+      } else {
+        FilterTitle += " by ";
+      }
+      FilterTitle += FilterValue;
+      id("filter-title").innerHTML = FilterTitle;
       let i = 0,
         obj;
       const num_objs = objs.length;
@@ -32,7 +47,7 @@ export function filter(element) {
         AddObj(objs[i], "filtered_data");
       }
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
     });
 }
